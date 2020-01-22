8941abd72b2daf7722e8772d22c5985fed424a0e
Zac Ingoglia <zacace@ingoglia.com>
2020-01-22 01:34:40 -0700

    V0.1
    HTML complete
    
    CSS layout complete

--- solver.js
+++ solver.js
@@ -1,10 +1,19 @@
 //These inital values are set outside of the loop function so that they don't get reset when the loop runs again
 var counter = 1;
 var temp = 4;
+var startingNum = "";
+var intervalNum = "";
+var instructions = document.getElementById("instructions"); //short hand for this command
 console.log("Arithmetic Sequence calculator for the number 4 if 1 is added to it 13 times"); // Adds context to the console
 
-loop(); //Runs the function below, this only triggers once
-function loop(){
+initInstructions();
+function initInstructions(){
+    instructions.innerHTML = "Enter the requested numbers, then click the operator to use";
+}
+
+
+solveLoop(); //Runs the function below, this only triggers once
+function solveLoop(){
     //Only increases the answer if the math has been run less than/equal to 13 times
     if(counter <= 12){
         var solve = temp + 1; // raises the value of temp by 1
@@ -12,7 +21,7 @@ function loop(){
         temp = solve; //Changes the value of the global var temp to be the value of the var + 1 as calculated with the num var
         // console.log("temp: " + temp); //Debug line for temp answer var
         // console.log("counter: " + counter); //Debug line for counter var
-        loop(); //repeats the above code in the if statement with the new counter and temp vars recorded
+        solveLoop(); //repeats the above code in the if statement with the new counter and temp vars recorded
     }
     //If the counter is at 13 or above it takes the last recorded temp value and puts it to a new var which is displayed in a console
     else{
